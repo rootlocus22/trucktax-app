85583416ed72d18b4a60bdda614a1000b827751f
rootlocus22 <rahuldubey220890@gmail.com>
2025-11-18 18:42:29 +0530

    fix build issue

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Image optimization settings
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+};
+
+module.exports = nextConfig;
+
