1ede54af4d8e0b0fd45f6cf0ddb83a188d3d2dc6
rootlocus22 <rahuldubey220890@gmail.com>
2025-12-25 23:06:36 +0530

    add more lessons

--- next.config.js
+++ next.config.js
@@ -6,12 +6,30 @@ const nextConfig = {
   },
   async redirects() {
     return [
+      // Redirect non-www to www (both HTTP and HTTPS)
       {
         source: '/:path*',
         has: [
           {
             type: 'host',
             value: 'quicktrucktax.com',
+          },
+        ],
+        destination: 'https://www.quicktrucktax.com/:path*',
+        permanent: true,
+      },
+      // Redirect HTTP www to HTTPS www
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.quicktrucktax.com',
+          },
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
           },
         ],
         destination: 'https://www.quicktrucktax.com/:path*',
